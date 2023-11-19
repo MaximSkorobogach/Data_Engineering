21e6eb1809730392bf7cd54a8d0b4f937b5c21f5
MaksimSkorobogach <skorobogach007@mail.ru>
2023-11-19 19:40:42 +0500

    Практика 1 Задание 2

--- Практическая работа 1/2/2.py
+++ Практическая работа 1/2/2.py
@@ -1,20 +1,20 @@
-def calculate_line_sums(file_path):
-    # Создаем список для хранения сумм по каждой строке
-    sums = []
+def calculate_line_averages(file_path):
+    # Создаем список для хранения средних значений по каждой строке
+    averages = []
 
     with open(file_path, 'r') as input_file:
         # Читаем каждую строку из файла
         for line in input_file:
             # Разбиваем строку на числа, используя точку с запятой в качестве разделителя
             numbers = [int(num) for num in line.split(';')]
-            # Считаем сумму чисел в строке
-            line_sum = sum(numbers)
-            # Добавляем сумму в список
-            sums.append(line_sum)
+            # Считаем среднее арифметическое чисел в строке
+            line_average = sum(numbers) / len(numbers)
+            # Добавляем среднее в список
+            averages.append(line_average)
 
-    # Записываем суммы по каждой строке в выходной файл
+    # Записываем средние значения по каждой строке в выходной файл
     with open('text_2_var_49_result', 'w') as output_file:
-        for line_sum in sums:
-            output_file.write(f'{line_sum}\n')
+        for line_average in averages:
+            output_file.write(f'{line_average}\n')
 
-calculate_line_sums('text_2_var_49')
+calculate_line_averages('text_2_var_49')
