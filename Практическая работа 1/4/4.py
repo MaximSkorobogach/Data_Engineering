fe6d63f8426617426935aeb1ac6a88ddcd06788a
MaksimSkorobogach <skorobogach007@mail.ru>
2023-11-19 20:46:43 +0500

    Практика 1 Задание 4

--- Практическая работа 1/4/4.py
+++ Практическая работа 1/4/4.py
@@ -2,7 +2,8 @@ import csv
 
 def process_csv(input_file_path, output_file_path):
     records = []
-#Создание записей
+
+    # Создание записей
     with open(input_file_path, newline="\n", encoding="utf-8") as file:
         reader = csv.reader(file, delimiter=",")
         for row in reader:
@@ -23,31 +24,30 @@ def process_csv(input_file_path, output_file_path):
 
     write_to_csv(output_file_path, sorted_records)
 
-#Расчет среднего по зарплате
+# Расчет среднего по зарплате
 def calculate_average_salary(records):
     total_salary = sum(record["salary"] for record in records)
     return total_salary / len(records) if len(records) > 0 else 0
 
-#Фильтрация записей
+# Фильтрация записей
 def filter_records(records, average_salary):
     return [
         record for record in records
-        #Фильтруем записи по 25 + (номер варианта mod 10)
+        # Фильтруем записи по 25 + (номер варианта mod 10)
         if record["salary"] > average_salary and record["age"] > 25 + (49 % 10)
     ]
 
-#Сортировка записей по номеру
+# Сортировка записей по номеру
 def sort_records(records):
     return sorted(records, key=lambda record: record["number"])
 
-#Внести записи в файл формата csv
+# Внести записи в файл формата csv
 def write_to_csv(output_file_path, records):
-    with open(output_file_path, 'w', encoding="utf-8", newline='') as file:
+    with open(output_file_path, 'w', encoding="utf-8-sig", newline='') as file:
         writer = csv.writer(file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         for record in records:
             record["salary"] = str(record["salary"]) + "₽"
             writer.writerow(record.values())
 
-# Пример использования
 process_csv('text_4_var_49', 'text_4_var_49_result')
