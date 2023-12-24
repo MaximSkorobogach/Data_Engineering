bfab26c07dad7de382a65bc3e2ef5cfe48106596
MaksimSkorobogach <skorobogach007@mail.ru>
2023-12-25 00:48:51 +0500

    Практика 3 Задание 2

--- Практическая работа 3/2/2.py
+++ Практическая работа 3/2/2.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 import numpy as np
 import json
 import shutil
-import re
 
+
+#Получить статистику числовую
 def get_num_stat(selector: str, items: list):
     nums = list(map(lambda x: float(x[selector]), items))
 
@@ -20,6 +21,7 @@ def get_num_stat(selector: str, items: list):
     return stat
 
 
+#Получить статистику по тексту
 def get_freq(selector: str, items: list):
     freq = {}
 
@@ -29,10 +31,14 @@ def get_freq(selector: str, items: list):
 
     return freq
 
+
+#Запись файла в JSON
 def write_to_json(path: str, data: str):
     with open(path, 'w', encoding="utf-8") as f:
         f.write(json.dumps(data, ensure_ascii=False))
 
+
+#Обработка файла
 def handle_file(file_name):
     items = list()
 
