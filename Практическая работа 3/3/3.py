558c4def777c305c1bd96512abbb4b702945cd38
MaksimSkorobogach <skorobogach007@mail.ru>
2023-12-25 00:36:47 +0500

    Практика 3 Задание 3

---
+++ Практическая работа 3/3/3.py
@@ -0,0 +1,86 @@
+import zipfile
+import os
+from bs4 import BeautifulSoup
+import numpy as np
+import json
+import shutil
+
+
+def get_num_stat(selector: str, items: list):
+    nums = list(map(lambda x: float(x[selector]), items))
+
+    stat = {}
+
+    stat['sum'] = sum(nums)
+    stat['min'] = min(nums)
+    stat['max'] = max(nums)
+    stat['avg'] = np.average(nums)
+    stat['std'] = np.std(nums)
+
+    return stat
+
+
+def get_freq(selector: str, items: list):
+    freq = {}
+
+    for item in items:
+        if selector in item:
+            freq[item[selector]] = freq.get(item[selector], 0) + 1
+
+    return freq
+
+
+def write_to_json(path: str, data: str):
+    with open(path, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(data, ensure_ascii=False))
+
+
+def handle_file(file_name):
+    with open(file_name, encoding="utf-8") as file:
+        text = ""
+        for row in file.readlines():
+            text += row
+
+        star = BeautifulSoup(text, 'xml').star
+
+        item = dict()
+        for el in star.contents:
+            if el.name is not None:
+                item[el.name] = el.get_text().strip()
+
+        return item
+
+
+# Путь к zip-архиву с HTML-файлами
+zip_path = 'zip_var_49.zip'  # Замените на реальный путь
+
+# Распаковка zip-архива
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall('temp_folder')
+
+items = []
+
+# Проходим по всем файлам в распакованной папке
+folder_path = 'temp_folder'
+for filename in os.listdir(folder_path):
+    if filename.endswith(".xml"):
+        result = handle_file(f'{folder_path}\{filename}')
+        items.append(result)
+
+# Удаляем временную папку с распакованными файлами
+shutil.rmtree(folder_path)
+
+items = sorted(items, key=lambda x: int(x['radius']), reverse=True)
+
+filtered_items = []
+for item in items:
+    if float(item['rotation'].replace(' days', '').strip()) >= 900:
+        filtered_items.append(item)
+
+num_stat = get_num_stat("radius", items)
+title_freq = get_freq("constellation", items)
+
+write_to_json("result_all.json", items)
+write_to_json("result_filtered.json", filtered_items)
+write_to_json("result_num_stat.json", num_stat)
+write_to_json("result_title_freq.json", title_freq)
